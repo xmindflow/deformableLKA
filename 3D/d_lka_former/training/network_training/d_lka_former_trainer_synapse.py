8d8e3fd478a99225837dfcf9cbc15bbc1b083513
Leon Niggemeier <leon.niggemeier@lfb.rwth-aachen.de>
2023-08-19 08:45:20 +0200

    changed epoch to 1000

--- 3D/d_lka_former/training/network_training/d_lka_former_trainer_synapse.py
+++ 3D/d_lka_former/training/network_training/d_lka_former_trainer_synapse.py
@@ -46,7 +46,7 @@ class d_lka_former_trainer_synapse(Trainer_synapse):
                  unpack_data=True, deterministic=True, fp16=False, trans_block=None, depths=[3,3,3,3], skip_connections=[True, True, True, True], seed=12345):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16, seed=seed)
-        self.max_num_epochs = 1#000
+        self.max_num_epochs = 1000
         self.initial_lr = 1e-2
         self.deep_supervision_scales = None
         self.ds_loss_weights = None
